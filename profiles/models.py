8410a0f8a715f0ad6b44c5ad511a42637806498c
showqi <mshowqi12@gmail.com>
2025-12-08 02:59:04 +0600

    userprofile updated

--- profiles/models.py
+++ profiles/models.py
@@ -8,9 +8,9 @@ class UserProfile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     email=models.EmailField(unique=True, null=True,blank=True)
-    skills = models.CharField(max_length=500, blank=True, null=True)  # comma-separated
+    skills = models.JSONField(default=list,blank=True,null=True)
     experience = models.TextField(blank=True, null=True)
-    career_interests = models.CharField(max_length=500, blank=True, null=True)
+    career_interests = models.JSONField(default=list,blank=True,null=True)
     cv_text = models.TextField(blank=True, null=True)
     cv_pdf = models.FileField(upload_to='cv_pdfs/', blank=True, null=True)
     pdf_text=models.JSONField(default=list,blank=True,null=True)
